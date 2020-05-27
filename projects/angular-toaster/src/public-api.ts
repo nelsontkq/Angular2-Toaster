3d6707206eb2ffcdb774d6fc9410ccc2540fed48
Nelson Truran <nelson@truran.dev>
2020-05-27 11:00:00 -0700

    angular 9

---
+++ projects/angular-toaster/src/public-api.ts
@@ -0,0 +1,12 @@
+/*
+ * Public API Surface of angular-toaster
+ */
+
+export { ToastComponent } from './lib/toast.component';
+export { ToasterContainerComponent } from './lib/toaster-container.component';
+export { ToasterService } from './lib/toaster.service';
+export { IClearWrapper } from './lib/clearWrapper';
+export { ToasterConfig, IToasterConfig } from './lib/toaster-config';
+export { Toast, ClickHandler, OnActionCallback } from './lib/toast';
+export { BodyOutputType } from './lib/bodyOutputType';
+export { ToasterModule } from './lib/toaster.module';
